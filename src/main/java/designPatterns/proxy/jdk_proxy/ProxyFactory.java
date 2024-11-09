f65f409d2c200c23842f72da7f5828e9b2ce5971
LiYaoYu <ErenJaegerKing@qq.com>
2024-11-09 12:31:10 +0800

    main:add proxy design pattern

--- src/main/java/designPatterns/proxy/jdk_proxy/ProxyFactory.java
+++ src/main/java/designPatterns/proxy/jdk_proxy/ProxyFactory.java
@@ -8,11 +8,36 @@ public class ProxyFactory {
 
     private TrainStation trainStation = new TrainStation();
 
+    //    public SellTickets getProxyObject() {
+//        SellTickets sellTickets = (SellTickets) Proxy.newProxyInstance(trainStation.getClass().getClassLoader(), trainStation.getClass().getInterfaces(), new InvocationHandler() {
+//            @Override
+//            public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+//                System.out.println("代理点收取一些服务费用");
+//                Object result = method.invoke(trainStation, args);
+//                return result;
+//            }
+//        });
+//        return sellTickets;
+//    }
     public SellTickets getProxyObject() {
+        //使用Proxy获取代理对象
+        /*
+            newProxyInstance()方法参数说明：
+                ClassLoader loader ： 类加载器，用于加载代理类，使用真实对象的类加载器即可
+                Class<?>[] interfaces ： 真实对象所实现的接口，代理模式真实对象和代理对象实现相同的接口
+                InvocationHandler h ： 代理对象的调用处理程序
+         */
         SellTickets sellTickets = (SellTickets) Proxy.newProxyInstance(trainStation.getClass().getClassLoader(), trainStation.getClass().getInterfaces(), new InvocationHandler() {
-            @Override
+            /*
+                InvocationHandler中invoke方法参数说明：
+                    proxy ： 代理对象
+                    method ： 对应于在代理对象上调用的接口方法的 Method 实例
+                    args ： 代理对象调用接口方法时传递的实际参数
+             */
             public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-                System.out.println("代理点收取一些服务费用");
+
+                System.out.println("代理点收取一些服务费用(JDK动态代理方式)");
+                //执行真实对象
                 Object result = method.invoke(trainStation, args);
                 return result;
             }
